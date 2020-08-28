ec901073664de06080c2397626a80cfc9b3f1092
juan <cjuan87@gmail.com>
2020-08-28 00:29:30 -0500

    first app backend

---
+++ Controllers/SolicitudController.cs
@@ -0,0 +1,29 @@
+﻿using Aplicacion.Interface;
+using Entidad;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace appbackend.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class SolicitudController : ControllerBase
+    {
+
+        private readonly ISolicitudAp _solicitud;
+        public SolicitudController(ISolicitudAp credito)
+        {
+            _solicitud = credito ?? throw new ArgumentException(nameof(credito));
+        }
+
+        [HttpGet]
+        public async Task<IEnumerable<SolicitudEnt>> get() {
+
+            var lista = await _solicitud.listar();
+            return lista;
+        }
+
+    }
+}
